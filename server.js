b9491b37a08403915b8751eaa236c785b8c3fccf
Ragillio Aji <lioajiragil@gmail.com>
2022-03-15 09:09:46 +0700

    add corsOptions

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const { logger } = require("./utils/logger");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
