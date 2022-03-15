604921e7d74cc91152e952e2be87fbb47c06cd99
Ragillio Aji <lioajiragil@gmail.com>
2022-03-15 09:19:53 +0700

    remove corsOptions

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ const { logger } = require("./utils/logger");
 const app = express();
 const port = process.env.PORT || 3001;
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
